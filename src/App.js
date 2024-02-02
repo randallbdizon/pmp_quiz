1d17e52efee12796de05fb45aa662d3c7fbc1321
Randall Dizon <randallbdizon@gmail.com>
2024-02-02 00:26:42 -0800

    Finished PMP_Questions.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ChatGPTv4.0: "Heh. Get rekt son."
         </p>
         <a
           className="App-link"
